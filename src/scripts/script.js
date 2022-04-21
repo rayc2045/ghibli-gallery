0ab358dc889002cdbf3dc11d6588ed2c4a0a96af
rayc2045 <>
2022-04-22 01:47:43 +0800

    Optimize code

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,38 +1,43 @@
-import works from '../data/works.js';
 import { createApp } from './petite-vue@0.4.1.js';
-import {
-  getParamsByUrl,
-  getRandomNum,
-  isVisible,
-  getLocalStorage,
-  setLocalStorage,
-  removeLocalStorageKey,
-} from './utils.js';
+import { getParamsByUrl, getRandomNum, isVisible } from './utils.js';
+import works from '../data/works.js';
 
-const localStorageKey = 'ghibli-album-id';
 const titleEl = document.querySelector('#title');
 const originalHref = window.location.href;
+
+const STORAGE_KEY = 'ghibli-album-id';
+const albumIdxStorage = {
+  fetch() {
+    return JSON.parse(localStorage.getItem(STORAGE_KEY));
+  },
+  save(id) {
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(id));
+  },
+  remove() {
+    localStorage.removeItem(STORAGE_KEY);
+  },
+};
 
 const App = {
   works,
   isLoading: true,
   isTopButtonHide: true,
-  currentIdx: 0,
+  currentAlbumIdx: 0,
   get language() {
     const { en } = getParamsByUrl();
     return en ? 'en' : 'jp';
   },
   get currentAlbum() {
-    return this.works[this.currentIdx];
+    return this.works[this.currentAlbumIdx];
   },
   get bodyStyle() {
     if (this.isLoading) return 'overflow: hidden;';
   },
   init() {
-    const localAlbumId = getLocalStorage(localStorageKey);
-    if (localAlbumId) return (this.currentIdx = localAlbumId);
-    this.currentIdx = getRandomNum(0, this.works.length);
-    this.saveLocalAlbumId();
+    const localAlbumIdx = albumIdxStorage.fetch();
+    if (localAlbumIdx) return (this.currentAlbumIdx = localAlbumIdx);
+    this.currentAlbumIdx = getRandomNum(0, this.works.length);
+    albumIdxStorage.save(this.currentAlbumIdx);
   },
   endLoading(sec) {
     setTimeout(() => (this.isLoading = false), sec * 1000);
@@ -44,23 +49,17 @@ const App = {
   moveToTop() {
     window.scrollTo(0, 0);
   },
-  getImgUrl(idx) {
+  getImageUrl(idx) {
     return `https://www.ghibli.jp/gallery/${
       this.currentAlbum.albumFolder
     }${String(idx + 1).padStart(3, '0')}.jpg`;
   },
   changeAlbum(idx) {
-    this.currentIdx = idx;
-    this.saveLocalAlbumId();
+    this.currentAlbumIdx = idx;
+    albumIdxStorage.save(this.currentAlbumIdx);
     setTimeout(() => {
       window.history.replaceState({}, '', originalHref);
     });
-  },
-  saveLocalAlbumId() {
-    setLocalStorage(localStorageKey, this.currentIdx);
-  },
-  removeLocalAlbumId() {
-    removeLocalStorageKey(localStorageKey);
   },
 };
 
@@ -72,5 +71,5 @@ window.onload = () => {
 };
 
 window.onscroll = () => App.handleScroll();
-window.onfocus = () => App.saveLocalAlbumId();
-window.onblur = () => App.removeLocalAlbumId();
+window.onfocus = () => albumIdxStorage.save(App.currentAlbumIdx);
+window.onblur = () => albumIdxStorage.remove();

2eef8df8ab0b5c17c9f25fb89de1b829491032c9
rayc2045 <>
2023-01-24 03:20:30 +0800

    Fix the function of getting query params

--- src/scripts/utils.js
+++ src/scripts/utils.js
@@ -1,12 +1,7 @@
-export function getParamsByUrl(url = window.location.href) {
-  const [urlSearch, params] = [url.split('?')[1], {}];
+export function getQueryParams(url = window.location.href) {
+  const urlSearch = url.split('?')[1];
   const urlSearchParams = new URLSearchParams(urlSearch);
-  for (const [key, value] of urlSearchParams.entries()) {
-    value
-      ? params[key] = value.split(' ')
-      : params[key] = [];
-  }
-  return params;
+  return Object.fromEntries(urlSearchParams.entries());
 }
 
 export function getRandomNum(min, max) {
